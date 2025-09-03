9804b9f298b7c6f91821adcb595b2e1d6a5ea129
Ananya54321 <ananyapappula521@gmail.com>
2025-09-03 15:40:46 +0530

    cleaned up unnecessary sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,9 @@ export default function Home() {
       <StatsSection />
       <DoctorsSection />
       <TestimonialsSection />
-      <AboutSection />
+      {/* <AboutSection /> */}
       <FAQSection />
-      <ContactSection />
+      {/* <ContactSection /> */}
       <Footer />
     </div>
   );
